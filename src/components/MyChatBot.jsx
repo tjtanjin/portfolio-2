6e01545412e0ad59d0c43fa0884957711669fb8c
tjtanjin <cjtanjin@gmail.com>
2025-12-16 00:46:39 +0800

    fix: Fix chatbot content

--- src/components/MyChatBot.jsx
+++ src/components/MyChatBot.jsx
@@ -1,14 +1,15 @@
-import { p } from "maath/dist/misc-7d870b3c.esm";
+import HtmlRenderer from "@rcb-plugins/html-renderer";
 import ChatBot from "react-chatbotify";
 
 const MyChatBot = () => {
     const user_options = ["Hobby", "Work", "Contact", "Resume"];
     const user = localStorage.getItem("welcome");
 
+    const plugins = [HtmlRenderer()];
+
     const settings = {
         botBubble: {
-            simStream: true,
-            dangerouslySetInnerHtml: true
+            simulateStream: true,
         },
         audio: {
             disabled: false
@@ -42,22 +43,25 @@ const MyChatBot = () => {
             }
         },
         hobby: {
-            message: "I spend a vast majority of my free time coding random things! <a href='https://spaceships.tjtanjin.com/' target='_blank'><u>Classic spaceships game</u></a>, " +
-                "<a href='https://www.spigotmc.org/resources/authors/tjtanjin.724342/' target='_blank'><u>minecraft plugins</u></a>, and even this very " +
-                "<a href='https://react-chatbotify.com' target='_blank'><u>chatbot</u></a> you are interacting with! 😊<br/><br/>" +
+            message: "I spend a vast majority of my free time exploring a bunch of projects! <a href='https://vibe-engineers.dev' target='_blank'><u>Vibe Engineers</u></a>, " + 
+                "<a href='https://spaceships.tjtanjin.com/' target='_blank'><u>Spaceships</u></a>, " +
+                "<a href='https://www.spigotmc.org/resources/authors/tjtanjin.724342/' target='_blank'><u>Minecraft Plugins</u></a>, and even this very " +
+                "<a href='https://react-chatbotify.com' target='_blank'><u>React ChatBotify</u></a> you are interacting with, just to name a few! 😊<br/><br/>" +
                 "When I am invested into a project, I really put in my efforts to make it the best that it can be!<br/><br/>" +
                 "Of course, there are times I ain't coding. You may or may not be interested to know that I am also pretty good at league of legends and minecraft 😝",
             chatDisabled: true,
             transition: {duration: 3000},
-            path: "prompt_again"
+            path: "prompt_again",
+            renderHtml: ["BOT"],
         },
         work: {
-            message: "I've worked on a wide range of projects during my work opportunities. This ranges from content management systems, to REST APIs to mobile applications.<br/><br/>" +
-                "As I also spend a great deal of time on my side projects, I have also dove into areas such as telegram bots, machine learning and server administration. In fact, keep looking and you will find some of my works on this website!<br/><br/>" +
+            message: "I've worked on a wide range of projects across the years. This ranges from content management systems, to REST APIs to mobile applications and platform engineering work.<br/><br/>" +
+                "As I also spend a great deal of time on side projects, I have also dove into areas such as telegram bots, AI agents and server administration. In fact, keep looking and you will find some of my works on this website!<br/><br/>" +
                 "I am a fast learner with an appetite for trying out new stuffs so having to deal with unfamiliar stuffs doesn't really bother me 😝",
             chatDisabled: true,
             transition: {duration: 3000},
-            path: "prompt_again"
+            path: "prompt_again",
+            renderHtml: ["BOT"],
         },
         contact: {
             message: "I can be contacted via the following platforms 😊<br/><br/>" +
@@ -65,17 +69,19 @@ const MyChatBot = () => {
                 "<li><a href='https://github.com/tjtanjin' target='_blank'><u>GitHub</u></a></li>" +
                 "<li><a href='https://www.linkedin.com/in/cjtanjin/' target='_blank'><u>LinkedIn</u></a></li>" +
                 "<li><a href='https://discord.gg/X8VSdZvBQY' target='_blank'><u>Discord</u></a></li>" +
-                "<li><a href='mailto:cjtanjin@gmail.com' target='_blank'><u>Email</u></a></li></ul>" +
+                "<li><a href='mailto:cjtanjin@gmail.com' target='_blank'><u>Email</u></a></li></ul><br/>" +
                 "Feel free to reach out! 😊",
             chatDisabled: true,
             transition: {duration: 3000},
-            path: "prompt_again"
+            path: "prompt_again",
+            renderHtml: ["BOT"],
         },
         resume: {
             message: "Sure thing! 😊 You can download my resume <a href='https://drive.google.com/uc?id=1mqG2V7AhDGvoajl2_nolMJr52YXQpp3Z&export=download' target='_blank'><u>here</u></a>",
             chatDisabled: true,
             transition: {duration: 3000},
-            path: "prompt_again"
+            path: "prompt_again",
+            renderHtml: ["BOT"],
         },
         show_options: {
             options: ["Hobby", "Work", "Contact", "Resume"],
@@ -102,7 +108,7 @@ const MyChatBot = () => {
 	}
 
     return (
-        <ChatBot settings={settings} flow={flow}/>
+        <ChatBot plugins={plugins} settings={settings} flow={flow}/>
     )
 }
 
